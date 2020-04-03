7aaa7ebde478ed1daeb72aa53ea3882c0f0b951c
Levi-Huynh <Levi_huynh@yahoo.com>
2020-04-02 20:41:38 -0700

    graph.py

--- notes/GRAPHSI.md
+++ notes/GRAPHSI.md
@@ -1,6 +1,29 @@
 # GRAPHS
 
-Graphs are collections of related data. They’re like trees, except connections can be made from any node to any other node, even forming loops. By this definition, all trees are graphs, but not all graphs are trees.
+Graphs are collections of related data. They’re like `trees`, except connections can be made from any node to any other node, even forming loops (not just L & R nodes)
+
+Components:
+`nodes / vertices` - repr objects in data set (cities, animals, web pages)
+`edges` - connections between vertices; can be bidrectional (subclasses of animals, from page a what can be accessed)
+`weight` - cost to travel across an edge  (resources may be diff to get from one node to another)
+
+# ways to implement graph
+# Diff sets
+# V ={vo, v1, v2, v3, v4, v5} verticies sets
+# E = {(vo,v1, 5), (v1, v2, 4) etc} edges sets (2 nodes connected, optional weight attribute)
+
+# application:
+- Location map: stops w/ diff nodes, diff trains (edges)
+- Network of activity, on GH- uses graph 
+
+
+
+
+# COLLECTIONs of relational data representd by nodes & _connections_ between nodes 
+-diff physical & conceptual networks
+
+
+ By this definition, all trees are graphs, but not all graphs are trees.
 
 We call the nodes in a graph vertexes (or vertices or verts), and we call the connections between the verts edges.
 
@@ -25,27 +48,31 @@ An edge denotes a relationship or linkage between the two verts.
 -It could represent the money owed in an ongoing poker night amongst friends.
 
 # Types of Graphs
-# Directed and Undirected Graphs
-# The nature of the relationship that is being represented is what determines if a directed or undirected graph should be used.  
 
-# Directed
+# Directed and Undirected Graphs
+(The nature of the relationship that is being represented is what determines if a directed or undirected graph should be used) 
+
+# Directed (is a weighted graph)
 If the relationship could be described as “one way”, then a directed graph makes the most sense. For example, representing the owing of money to others (debt) with a directed graph would make sense.
 -A directed graph has at `least one edge` that is not `bidirectional`. 
 -So, again, we can draw our 7 verts and then connect them with edges. This time, we just need to make sure that one of the edges is an arrow pointing in only one direction.
 ![Directed](directed.png)
 
-# Undirected
+# Undirected(is a weighted graph)
 -If the nature of the relationship is a _mutual exchange_, then an undirected graph makes the most sense. 
 -For example, we could use an undirected graph to represent users who have exchanged money at some point in the past. 
 -Since an “exchange” relationship is always mutual, an undirected graph makes the most sense here.
+![undirected](undirected.png)
 
-# Cyclic Graphs
+# Cyclic Graphs (edges allow you to visit at least one vert/node) Some can have `unweighted` edges
 - If you can form a cycle (for example, follow the edges and arrive again at an already-visited vert), the graph is cyclic. 
 -For example, in the image below, you can start at B and then follow the edges to C, E, D, and then back to B (which you’ve already visited).
 
-#  Acyclic 
+#  Acyclic(vertices can only be visited once) 
 - cannot form a cycle (for example, you cannot arrive at an already-visited vert by following the edges), we call the graph acyclic. 
 -In the example below, no matter which vert you start at, you cannot follow edges in such a way that you can arrive at an already-visited vert.
+
+
 
 ![Acyclic](acyclic.png)
  
@@ -81,3 +108,69 @@ A DAG has several applications.` DAGs can modely many different kinds of informa
 # ex 3
 # cyclic directed graph (5 verts)
 ![cyclicDirected](cyclicDirected.png)
+
+
+# 4 
+# acyclic graph (Directed Acyclic Graph DAG) 9 verts
+![DAG](DAG4.png)
+
+-acyclic—we can order a DAG’s vertices linearly in such a way that every edge is directed from earlier to later in the sequence.
+-For this graph, we will draw our 9 verts in a line from left to right. Then, we will draw our edges making sure that the edges always point from left to right (earlier to later in the sequence).
+
+# Adjancy List
+`Notice that this adjacency list doesn’t use any lists. The vertices collection is a dictionary which lets us access each collection of edges in O(1) constant time. Because the edges are contained in a set we can check for the existence of edges in O(1) constant time`
+
+class Graph:
+    def __init__(self):
+        self.vertices = {
+                            "A": {"B"},
+                            "B": {"C", "D"},
+                            "C": {"E"},
+                            "D": {"F", "G"},
+                            "E": {"C"},
+                            "F": {"C"},
+                            "G": {"A", "F"}
+                        }
+
+
+# Adjency Matrix
+class Graph:
+    def __init__(self):
+        self.edges = [[0,1,0,0,0,0,0],
+                      [0,0,1,1,0,0,0],
+                      [0,0,0,0,1,0,0],
+                      [0,0,0,0,0,1,1],
+                      [0,0,1,0,0,0,0],
+                      [0,0,1,0,0,0,0],
+                      [1,0,0,0,0,1,0]]
+
+We represent this matrix as a two-dimensional array–a list of lists. With this implementation, we get the benefit of `built-in edge weights`. `0 denotes no relationship`, but `any other value that is present represents an edge label or edge weight`. The drawback is that we do not have a built-in association between the vertex values and their index.
+
+`In practice, implementing both the adjacency list and adjacency matrix would contain more information by including Vertex and Edge classes.`
+
+
+
+Shorthand	Property
+V	Total number of vertices in the graph
+E	Total number of edges in the graph
+e	Average number of edges per vertex
+Space Complexity
+Adjacency Matrix
+Complexity: O(V^2) space
+---------------------------------------------------------------
+
+Consider a dense graph where each vertex points to each other vertex. Here, the total number of edges will approach V^2. This means that regardless of whether you choose an adjacency list or an adjacency matrix, both will have a comparable space complexity. However, dictionaries and sets are less space efficient than lists. So, for dense graphs (graphs with a high average number of edges per vertex), the adjacency matrix is more efficient because it uses lists instead of dictionaries and sets.
+---------------------------------------------------------------------------
+
+Add Vertex
+Adjacency Matrix
+Complexity: O(V) time
+
+For an adjacency matrix, we would need to add a new value to the end of each existing row, then add a new row at the end.
+
+for v in self.edges:
+  self.edges[v].append(0)
+v.append([0] * len(self.edges + 1))
+Remember that with Python lists, appending to the end of a list is O(1) because of over-allocation of memory but can be O(n) when the over-allocated memory fills up. When this occurs, adding the vertex can be O(V^2).
+
+---------------------------------------------------------------
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,62 +1,207 @@
 """
 Simple graph implementation
+
+class notes:
+sets & dicts are HT under hood
+
+{
+    '0': {'1', '3'},
+    '1': {'0'},
+    '2': set(),
+    '3': {'0'}
+}
+^ dict of sets
+
+-undirected/ bidirectional edges (0 points 1, 1 points 0)
+(0 point 3, 3 points 0)
+-.add_edge to node that doesn't exist, get exception
+
+-for this problem all directed edges
+
 """
 from util import Stack, Queue  # These may come in handy
+
 
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
+
         """
-        pass  # TODO
+        # just add another row to our vertices
+        # self.vertices[new row] = empty set
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # check if v1 & v2 exist as vertice
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            print("can't add to vertices that dont exist")
+
+    def add_undirected_edge(self, v1, v2):
+        """
+        Add a un directed edge to the graph.
+        """
+        # check if v1 & v2 exist as vertice
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            print("can't add to vertices that dont exist")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        else:
+            print("Vertex id does not exist")
+            raise ValueError("Vertex id does not exist")
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
+        -Look at everything thats 1 away #does not have to be looked at in order L->R
+        -Look at everything thats 2 away # ^
+        -Look at everything thats 3 away  # ^
+        etc...
+
+        # create a queue FIFO
+        # enqueue the starting vertix
+        # create a set to store visited vertices
+        # while queue is not empty:
+                -Dequeue the first vertex
+                # when dequeue, (queueing allows u to check if its been vistied in order) (check if in set?)
+                # mark it as visited (if not, add to set) If has been visited, ignore, deque the next vertix in queue
+                # enqueue all its neighbors (add its children BDT children to end of queue)
+                    # (1 away, 2 away, 3 away ) 
+                    # deque the next vert in FIFO order 
+                    # check if been visited
+                    # mark it as visited 
+                    # enqueu its nieghbors /children (add to end of queue)
+
+        q= []
+        visited = {1,2,4,4,6,7,5}
+        memorize pseduo, not code
+
         """
-        pass  # TODO
+        # create a queue FIFO
+        q = Queue()
+        # enqueue the starting vertix
+        q.enqueue(starting_vertex)
+        # create a set to store visited vertices
+        visited = set()
+        # while queue is not empty:
+        while q.size() > 0:
+            # Dequeue the first vertex
+            v = q.dequeue()
+            # when dequeue, (queueing allows u to check if its been vistied in order)
+            # (check if in set?)
+            # mark it as visited (if not, add to set) If has been visited (else ignore, deque the next vertix in queue)
+            if v not in visited:
+                print(v)
+                visited.add(v)
+            # enqueue all its neighbors (add its children BDT children to end of queue)
+                for neighbor in self.get_neighbors(v):
+                    q.enqueue(neighbor)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
-        """
-        pass  # TODO
 
-    def dft_recursive(self, starting_vertex):
+        DFT recursive application fits well:
+
+        mark as visited
+        do the thing (print, modify, do action)
+        DFT traversal on each of children (parent of children wont return
+        until traversal completed for children. left children always traversed
+        first. 
+        -PARENT stack will continue, after `all` its children return
+        -DFT calls of parent & its 2nd child wont start until all the children's children children
+        of the 1st child is completed/visted (in case where theres 2 childs of a parent) )
+        -if its _been_ visted that node can return 
+
+           # create a stack FILO/ LIFO (like pancake) related to RECURSION
+        # PUSH the starting vertix to stack
+        # create a set to store visited vertices
+        # while stack is not empty:
+                # Pop the vertex from back/end of stack
+                # Check if its been visited
+                # if it hasn't been visited:
+                    # Mark it as visited
+                    # Push all its neighbors onto the stck 
+
+        # optimazations are tradeoffs based on the constraints of your system for time/space
+        its all relative based on what kind of system youre operating in 
+
+        """
+        # create a stack FILO/ LIFO (like pancake) related to RECURSION
+        s = Stack()
+        # PUSH the starting vertix to stack
+        s.push(starting_vertex)
+        # create a set to store visited vertices
+        visited = set()
+        # while stack is not empty:
+        while s.size() > 0:
+            # Pop the vertex from back/end of stack
+            v = s.pop()
+            # Check if its been visited
+            # if it hasn't been visited:
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Push all its neighbors onto the stck
+                for neighbor in self.get_neighbors(v):
+                    s.push(neighbor)
+
+    # add default arg python gotchas docs.python.guide
+    def dft_recursive(self, starting_vertex, visted=None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        # check if not node has been visited
+        # if not...
+        # mark it as visited #how to carry out visited set thru all recursive calls
+        # nested recursive function
+        # implement a helper
+
+        # call dft_recursive on each neighbor
 
     def bfs(self, starting_vertex, destination_vertex):
         """
-        Return a list containing the shortest path from
+        Return a list `PATH` containing the shortest path from
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        # create a queue FIFO
+        # enqueue A PATH to starting vertix
+        # create a set to store visited vertices
+        # while queue is not empty:
+        # Dequeue the first path
+        # GRAB THE VERTEX FROM END OF PATH
+        # check if its been visted
+        # If hasn't been visited:
+        # mark it as visisted
+        # enquene A PATH to all its neighbors
+        # MAKE A COPY OF THE PATH
+        # ENQUE THE COPY
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -76,6 +221,7 @@ class Graph:
         """
         pass  # TODO
 
+
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
     # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
@@ -86,7 +232,7 @@ if __name__ == '__main__':
     graph.add_vertex(5)
     graph.add_vertex(6)
     graph.add_vertex(7)
-    graph.add_edge(5, 3)
+    graph.add_edge(5, 3)  # first arg is start node, 2nd arg is end node
     graph.add_edge(6, 3)
     graph.add_edge(7, 1)
     graph.add_edge(4, 7)
@@ -98,10 +244,10 @@ if __name__ == '__main__':
     graph.add_edge(4, 6)
 
     '''
-    Should print:
+    Should print: adjacent list 
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
-    print(graph.vertices)
+    # print(graph.vertices)
 
     '''
     Valid BFT paths:
@@ -118,28 +264,33 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    graph.bft(1)
+    # graph.bft(1)
 
     '''
-    Valid DFT paths:
+    Valid DFT paths: (many)
         1, 2, 3, 5, 4, 6, 7
         1, 2, 3, 5, 4, 7, 6
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
     graph.dft(1)
-    graph.dft_recursive(1)
+    # graph.dft_recursive(1)
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    print(graph.bfs(1, 6))
+    # print(graph.bfs(1, 6))
 
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
+    # print(graph.dfs(1, 6))
+    # print(graph.dfs_recursive(1, 6))
+
+    # once build graph, get insight by analyzing relationships
+    # neural networks, all graph traversals
+    # nodes, relationships between nodes, which edges are more strongly correlated, neural netork
+    # traversing will give you insights
